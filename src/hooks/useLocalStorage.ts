68c140bb739b8f36e0f3743c753104ad7c3a0000
Yariv Gilad <yariv.gilad@gmail.com>
2025-08-20 07:29:38 +0300

    updated readme & hook

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,45 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 
-export function useLocalStorage<T>(
-  KEY: string,
-  initialValue: T
-): [T, (val: T | ((val: T) => T)) => void] {
-
+export function useLocalStorage<T>(KEY: string,initialValue: T): [T, (val: T | ((val: T) => T)) => void] {
+  
   const [storedValue, setStoredValue] = useState<T>(() => {
     const item = localStorage.getItem(KEY);
     if (item !== null) return JSON.parse(item) as T;
     return initialValue;
   });
 
-  // const setValue = (value: T) => {
-  //   setStoredValue(value);
-  //   localStorage.setItem(APP_NAME, JSON.stringify(value));
-  // };
-  type cb = (val: T) => T
-
-  const setValue = (value: T | cb) => {
-    let newValue: T;
-    if (typeof value === "function") {
-      newValue = (value as cb)(storedValue);
-    } else {
-      newValue = value;
-    }
-    setStoredValue(newValue);
-    localStorage.setItem(KEY, JSON.stringify(newValue));
-    //return newValue;
-  };
-
-  return [storedValue, setValue];
+  useEffect(() => {
+    localStorage.setItem(KEY, JSON.stringify(storedValue));
+  }, [storedValue]);
+  
+  return [storedValue, setStoredValue];
 }
-//
-// if we want to support passing a callback to the setValue
-//-----------------------------------------------------------
-// const setValue = (value: any) => {
-//   let newValue = value;
-//   if(typeof value === 'function'){
-//     newValue = value(storedValue)
-//   }
-//   setStoredValue(newValue);
-//   localStorage.setItem(APP_NAME, JSON.stringify(newValue));
-// }
+
+
+
+/* 
+
+type cb = (val: T) => T
+
+const setValue = (value: T | cb) => {
+  let newValue: T;
+  if (typeof value === "function") {
+    newValue = (value as cb)(storedValue);
+  } else {
+    newValue = value;
+  }
+  setStoredValue(newValue);
+  localStorage.setItem(KEY, JSON.stringify(newValue));
+  //return newValue;
+}; 
+*/
